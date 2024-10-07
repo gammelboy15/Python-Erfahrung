c8e88f4fac39f6e5033568f97d1ed8f923e3cde1
gammelboy15 <99133060+gammelboy15@users.noreply.github.com>
2024-10-07 17:47:38 +0200

    Add files via upload

---
+++ gabriel El Bendary Mandala.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("green")
+turtle.Screen().bgcolor("black")
+turtle.pensize(2)
+turtle.speed(25)
+
+
+for Mandala in range(32):
+        turtle.right(20)
+        turtle.forward(200)
+        turtle.right(90)
+        turtle.forward(200)
+        turtle.right(90)
+        turtle.forward(200)
+        turtle.right(90)
+        turtle.forward(200)
+
+turtle.hideturtle
+
